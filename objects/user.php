5a1761c62836fd14321b4936c5c23a43ac1a7b0b
Rio Gunawan <riogunawan8967uajy@gmail.com>
2018-09-16 15:57:36 +0700

    email desain

--- objects/user.php
+++ objects/user.php
@@ -116,8 +116,8 @@ class User{
     }
     function mail(){
         $email_subject = "Website Contact From:  Tiem Schedule";
-            $headers = "From: tiemschedule@tiematweb18.000webhostapp.com"."\r\n";
-            $headers .= "Reply-To:noreply@tiematweb18.000webhostapp.com" . "\r\n";
+            $headers = "From: tiemschedule@tiemschedule.thekingcorp.org"."\r\n";
+            $headers .= "Reply-To:noreply@tiemschedule.thekingcorp.org" . "\r\n";
             $headers .= "MIME-Version: 1.0\r\n";
             $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
             
@@ -129,11 +129,11 @@ class User{
                     $message .='</div>';
                     $message .='<div class="mail-body" style="color: black; background-color:  #CFE7EA; width: 100%; padding: 20px;">';
                         $message .='<h1>Hallo '.$this->nama.', Silahkan lakukan verifikasi email anda dengan menekan tombol berikut </h1>';
-                        $message .='<a href="https:/tiematweb18.000webhostapp.com/mail/verifikasiEmail.php?token='.$this->token.'"><button style="background-image: linear-gradient(to left, #0025BC , #0071BC); width: 100%; text-align: center; margin: auto; min-height: 40px; color: white; font-size: 30px; cursor: pointer;">Klik disini</button></a>';
+                        $message .='<a href="https://tiemschedule.thekingcorp.org/mail/verifikasiEmail.php?token='.$this->token.'"><button style="background-image: linear-gradient(to left, #0025BC , #0071BC); width: 100%; text-align: center; margin: auto; min-height: 40px; color: white; font-size: 30px; cursor: pointer;">Klik disini</button></a>';
                     $message .='</div>';
                     $message .='<div class="mail-footer" style="color: black; background-color: #adadad; width: 100%; font-size: 20px;padding: 20px; border-bottom-left-radius: 25px; border-bottom-right-radius: 25px;">';
                         $message .='Apabila link tersebut bermasalah, silahkan akses url berikut:';
-                        $message .='https://tiematweb18.000webhostapp.com/mail/verifikasiEmail.php?token='.$this->token;
+                        $message .='https://tiemschedule.thekingcorp.org/mail/verifikasiEmail.php?token='.$this->token;
                     $message .='</div>';
                 $message .='</div>';
             $message .='</body>';
