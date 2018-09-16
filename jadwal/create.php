455d7aad11f567eaf263a20e9c37629c944fb079
Rio Gunawan <riogunawan8967uajy@gmail.com>
2018-09-16 12:23:36 +0700

    datejadwal

--- jadwal/create.php
+++ jadwal/create.php
@@ -9,7 +9,7 @@ $product = new Jadwal($db);
  
 $product->jadwal = $_POST["jadwal"];
 $product->waktu = $_POST["waktu"];
-$product->tanggal = date('Y-m-d',$_POST["tanggal"]);
+$product->tanggal = date_format('d/m/Y',$_POST["tanggal"]);
 $product->tempat = $_POST["tempat"];
 $product->prioritas = $_POST["prioritas"];
 $product->user_id = $_POST["id"];
