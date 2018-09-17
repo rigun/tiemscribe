3b31965845f0a85bf9176ec68a03087536a07804
Rio Gunawan <riogunawan8967uajy@gmail.com>
2018-09-17 21:34:51 +0700

    delete

--- user/delete.php
+++ user/delete.php
@@ -1,13 +1,4 @@
 <?php
-// required headers
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST, DELETE");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
- 
- 
-// include database and object file
 include_once '../config/database.php';
 include_once '../objects/user.php';
  
@@ -18,11 +9,7 @@ $db = $database->getConnection();
 // prepare product object
 $product = new User($db);
  
-// get product id
-$data = json_decode(file_get_contents("php://input"));
- 
-// set product id to be deleted
-$product->id = $data->id;
+$product->id = $_POST["id"];
  
 // delete the product
 if($product->delete()){
