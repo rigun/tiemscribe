89ddd860eae8f5ae685e35849e5e11a513a88826
Rio Gunawan <riogunawan8967uajy@gmail.com>
2018-09-22 09:02:54 +0700

    password verif

---
+++ user/updatePasswordById2.php
@@ -0,0 +1,29 @@
+<?php
+// include database and object files
+include_once '../config/database.php';
+include_once '../objects/user.php';
+ 
+// get database connection
+$database = new Database();
+$db = $database->getConnection();
+ 
+// prepare product object
+$product = new User($db);
+ 
+$product->id = $_POST['id'];
+$product->password = $_POST['password'];
+
+if($product->updatePasswordById2()){
+    $response["value"] = 200;
+    $response["message"] = "Password Berhasil di Perbaharui";
+    echo json_encode($response);
+}
+    
+// if unable to update the product, tell the user
+else{
+    $response["value"] = 0;
+    $response["message"] = "Password Gagal di Perbaharui";
+    echo json_encode($response);
+}
+
+?>
