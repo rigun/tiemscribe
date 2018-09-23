d6f8a2fe9310e4d0f52bc386dfb942cbb273679a
Rio Gunawan <riogunawan8967uajy@gmail.com>
2018-09-23 23:29:01 +0700

    update

--- mail/verifikasiEmail.php
+++ mail/verifikasiEmail.php
@@ -16,7 +16,7 @@ $product->token = isset($_GET['token']) ? $_GET['token'] : die();
 if($product->updateByToken()){
     echo '<h1>Verifikasi Berhasil</h1><br/>';
     echo '<hr><br/>';
-    echo '<h2 style="font-weight:normal">Selamat, Email anda berhasil di verifikasi,<br/> sekarang anda sudah bisa untuk melakukan login dan menikmati fitur-fitur dari <strong>TiEm Schedule</strong></h2>';
+    echo '<h2 style="font-weight:normal">Selamat, Email anda berhasil di verifikasi.<br/> sekarang anda sudah bisa untuk melakukan login dan menikmati fitur-fitur dari <strong>TiEm Schedule</strong></h2>';
 
 }
 else{
