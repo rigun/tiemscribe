75434742bbe05277f687214ab45d6a88064c617a
Rio Gunawan <riogunawan8967uajy@gmail.com>
2018-09-23 23:25:46 +0700

    update

--- mail/verifikasiEmail.php
+++ mail/verifikasiEmail.php
@@ -1,11 +1,4 @@
 <?php
-// required headers
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: PUT, PATCH, POST");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
- 
 // include database and object files
 include_once '../config/database.php';
 include_once '../objects/user.php';
